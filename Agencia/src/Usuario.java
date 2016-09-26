fe376a04d511ef2f519b6a0e3a0924d9d8330db9
Usuario <Usuario@NetPC>
2016-09-26 09:12:37 -0300

    Agencia

---
+++ Agencia/src/Usuario.java
@@ -0,0 +1,17 @@
+
+public class Usuario {
+
+	String usuario;
+	String contraseña;
+	int terminal;
+		
+//	public Usuario(String usuario, String contraseña, int terminal) {
+//		this.usuario = usuario;
+//		this.contraseña = contraseña;
+//		this.terminal = terminal;
+//	}
+	
+	
+		
+		
+}
