4647a64c91f532555a5aa3683dabcd477d7d1874
Usuario <Usuario@NetPC>
2016-10-02 22:15:21 -0200

    Armando el modelo de la agencia

--- Agencia/src/Usuario.java
+++ Agencia/src/Usuario.java
@@ -1,17 +1,32 @@
 
 public class Usuario {
 
-	String usuario;
-	String contraseña;
-	int terminal;
+	private String usuario;
+	private String contraseña; //ver como cifrar clave al guardar en base
+	private int terminal;
+	
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+	public int getTerminal() {
+		return terminal;
+	}
+	public void setTerminal(int terminal) {
+		this.terminal = terminal;
+	}
+	
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
 		
-//	public Usuario(String usuario, String contraseña, int terminal) {
-//		this.usuario = usuario;
-//		this.contraseña = contraseña;
-//		this.terminal = terminal;
-//	}
-	
-	
+
 		
 		
 }
