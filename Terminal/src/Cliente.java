78f9b1c8be99a5ab8e2a6391dd9292d38ac5320f
Usuario <Usuario@NetPC>
2016-10-12 20:10:53 -0200

    Se agrega Terminal (cliente socket)

---
+++ Terminal/src/Cliente.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Cliente {
+
+	static final String host = "localhost";
+	static final int puerto = 5000;
+	
+	public Cliente() {
+		try {
+			Socket skCliente = new Socket(host,puerto);
+			OutputStream out = skCliente.getOutputStream();
+			DataOutputStream salida = new DataOutputStream(out);
+			InputStream in = skCliente.getInputStream();
+			DataInputStream entrada = new DataInputStream(in);
+			Scanner ent = new Scanner(System.in);
+			
+			String mensaje;
+			String usuario;
+			String password;
+			
+			do {
+				
+				System.out.println(entrada.readUTF());
+				usuario = ent.next();
+				salida.writeUTF(usuario);
+				System.out.println(entrada.readUTF());
+				password = ent.next();
+				salida.writeUTF(password);
+				
+				mensaje = entrada.readUTF();
+				System.out.println("Resultado de login " + mensaje);
+				
+			} while (!(mensaje.equals("ok")));
+			
+			if (mensaje.equals("ok")) {
+				
+				String comando;
+				
+				do {
+					System.out.println(entrada.readUTF());
+					comando = ent.next();
+					salida.writeUTF(comando);
+					
+					switch (comando) {
+						
+						case "1": {
+							System.out.println(entrada.readUTF());
+							String matricula = ent.next();
+							salida.writeUTF(matricula);
+							System.out.println(entrada.readUTF());
+							String fecha = ent.next();
+							salida.writeUTF(fecha);
+							System.out.println(entrada.readUTF());
+							String min = ent.next();
+							salida.writeUTF(min);
+							System.out.println(entrada.readUTF());
+							break;
+						}
+						
+						case "2": {
+							System.out.println(entrada.readUTF());
+							String num = ent.next();
+							salida.writeUTF(num);
+							System.out.println(entrada.readUTF());
+							break;
+						}
+						
+						case "3": {
+							System.out.println(entrada.readUTF());
+							entrada.close();
+							salida.close();
+							skCliente.close();
+							ent.close();
+							System.exit(0);
+							break;
+						}
+						
+						default : { System.err.println("\n Ingrese un comando válido\n"); break; }
+					}
+				} while (comando != "3");
+			}
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Cliente();
+
+	}
+
+}
